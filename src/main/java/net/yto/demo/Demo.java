83284123f2ec242a36d289aa86455d9229b66f7f
liufei <liufei-vip@163.com>
2019-01-12 22:03:54 +0800

    测试冲突

--- src/main/java/net/yto/demo/Demo.java
+++ src/main/java/net/yto/demo/Demo.java
@@ -2,6 +2,6 @@ package net.yto.demo;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git22!");
     }
 }
