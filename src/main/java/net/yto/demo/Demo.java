5544fb1e8210d50f04c34709a2e011aab4527971
01530720 <01530720@yto.net.cn>
2019-01-12 22:07:19 +0800

    测试冲突

--- src/main/java/net/yto/demo/Demo.java
+++ src/main/java/net/yto/demo/Demo.java
@@ -2,6 +2,6 @@ package net.yto.demo;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello Git444!");
+        System.out.println("Hello Git555!");
     }
 }
